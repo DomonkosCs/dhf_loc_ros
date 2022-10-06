da0049297d23b21177e4968d9914402439576bbe
Domonkos Csuzdi <csuzdi02@gmail.com>
2022-10-06 09:36:15 +0200

    refactor: formatting

--- dhf_loc/setup.py
+++ dhf_loc/setup.py
@@ -4,6 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=["dhflocalization"], package_dir={"": "include"},
+    packages=["dhflocalization"],
+    package_dir={"": "include"},
 )
 setup(**setup_args)
