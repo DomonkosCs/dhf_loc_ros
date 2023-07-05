e7e21596b3017eebb0f474b2709523e4f8bcdae4
DomonkosCs <domonkos@csuzdi.hu>
2023-07-05 12:04:29 +0200

    feat: add dhfloc library as submodule

--- dhf_loc/setup.py
+++ dhf_loc/setup.py
@@ -4,6 +4,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=["dhflocalization"], package_dir={"": "include"},
+    packages=["dhflocalization"], package_dir={"": "include/dhf-localization"},
 )
 setup(**setup_args)
